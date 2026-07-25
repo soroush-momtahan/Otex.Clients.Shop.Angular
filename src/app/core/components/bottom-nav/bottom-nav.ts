3e624881a50d5c10e0b3b3bd437622f8a1f314f8
soroush-momtahan <soroush.momtahan@gmail.com>
2026-07-25 10:54:01 +0330

    change bottom nav

--- src/app/core/components/bottom-nav/bottom-nav.ts
+++ src/app/core/components/bottom-nav/bottom-nav.ts
@@ -17,9 +17,11 @@ export class BottomNav {
 
   // لیست آیتم های منو
   navItems = signal([
-    { id: 'home', label: 'خانه', icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' },
-    { id: 'cart', label: 'سبد خرید', icon: 'M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z' },
-    { id: 'profile', label: 'پروفایل', icon: 'M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z' },
+    { id: 'home', label: 'خانه'},
+    { id: 'products', label: 'محصولات'},
+    { id: 'wallet', label: 'کیف‌پول'},
+    { id: 'cart', label: 'سبدخرید'},
+    { id: 'profile', label: 'پروفایل'},
   ]);
 
   checkLoginAndSetActiveAndNavigate(tabId: string) {
